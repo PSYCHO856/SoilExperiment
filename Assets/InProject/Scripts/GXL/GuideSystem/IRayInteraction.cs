475490467b7943318ed2a785fa490c23554e066f
xian <charleslee6048az@gmail.com>
2023-02-06 00:29:33 +0800

    26

--- Assets/InProject/Scripts/GXL/GuideSystem/IRayInteraction.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using UnityEngine;
-/// <summary>
-/// 射线交互接口
-/// </summary>
-public interface IRayInteraction 
-{
-    void OnHover(RaycastHit hitInfo);
-}
--- Assets/InProject/Scripts/GXL/UI/Tool/FUIToolUnit.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine.UI;
-//道具元件1
-public class FUIToolUnit : UnitBase
-{
-   public Text title;
-   public Image icon;
-   public Toggle tg;
-   
-}
